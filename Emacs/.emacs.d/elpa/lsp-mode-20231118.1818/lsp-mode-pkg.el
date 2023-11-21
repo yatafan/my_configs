9eae1208289de80279271664775d4e21698409de
yatafan <nadgob358@gmail.com>
2023-11-21 09:08:36 +0300

    my first config

---
+++ Emacs/.emacs.d/elpa/lsp-mode-20231118.1818/lsp-mode-pkg.el
@@ -0,0 +1,21 @@
+(define-package "lsp-mode" "20231118.1818" "LSP mode"
+  '((emacs "27.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0")
+    (eldoc "1.11"))
+  :commit "cb2b565b4e9b38be1f4c284e7618160383ad665d" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainers
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
